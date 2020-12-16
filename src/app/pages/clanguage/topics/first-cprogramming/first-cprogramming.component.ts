475c88e918ed07cbb50272ccd3e778a9ebab8eb1
sukantahui <huisukanta77@gmail.com>
2020-12-16 13:59:29 +0530

    section added

--- src/app/pages/clanguage/topics/first-cprogramming/first-cprogramming.component.ts
+++ src/app/pages/clanguage/topics/first-cprogramming/first-cprogramming.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgxWatermarkOptions} from 'ngx-watermark';
 
 @Component({
   selector: 'app-first-cprogramming',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-cprogramming.component.scss']
 })
 export class FirstCProgrammingComponent implements OnInit {
-
+  options: NgxWatermarkOptions = {
+    text: 'CODER\nSimple & Charming\nLife is great',
+    width: 350,
+    height: 300,
+    fontFamily: 'Kanit',
+    color: '#999',
+    alpha: 0.4,
+    degree: -45,
+    fontSize: '20px',
+  };
   constructor() { }
 
   ngOnInit(): void {
